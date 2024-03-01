dfb7c73674a7a68fd723130d576a319c2b93290f
FacuM <facumo.fm@gmail.com>
2024-03-01 08:28:00 -0300

    entrypoint: avoid usage of symlinks

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,14 @@ set -e; # quit on error
 
 composer install;
 
+mkdir -p storage/app;
+
+cp -rfv docs/* storage/app;
+
 php application app:update-availability-options;
 php application app:update-schedule;
 php application app:update-train-stations-map;
 php application app:update-holidays-list;
 php application app:update-hash-list;
+
+cp -rfv storage/app/* docs/;
